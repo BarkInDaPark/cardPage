28e6c54dd632a2c162b2603ece28df7276dccb90
BarkInDaPark <matte_skater@hotmail.com>
2025-03-25 00:09:11 +0100

    added button navigating to /cards and css style

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import logo from './monocle.png';
 import './Homepage.css';
 import Navbar from '../components/Navbar'; // Import the Navbar component
+import Cards from '../components/Cards';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Homepage() {
+    const navigate = useNavigate();
     return (
         <div className='homepage'>
             <Navbar /> {/* Add the Navbar component */}
-            <h1>Welcome to gents rooms</h1>
-            <img src={logo} alt="Logo" className="spin-logo" />
+            <button className= 'button' onClick={() => navigate('/cards')}>Enter</button>
+            <h1>
+                < img src={logo} alt="Logo" className="spin-logo" />
+                Gents club!
+            </h1>
+           
         </div>
     );
 }
