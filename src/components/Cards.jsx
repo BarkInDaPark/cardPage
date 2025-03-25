25d851fc3f03458b0005c9081e6cf56fe1285b53
BarkInDaPark <matte_skater@hotmail.com>
2025-03-25 12:56:37 +0100

    added a map function to get more cards for debugging

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,13 +5,18 @@ function Cards({ name }) {
     if (!name){
         name = "no name";
     }
+    const cardNameArr = ["BlackJack", "car", "Run", "draw", "jump"];
+    
     return (
-        <div className="card"
-        key={name}>
-            {name}
-            
+        <div className = "card-container">
+            {cardNameArr.map((names) => (
+            <div className="card"
+            key={names}>
+            {   names}
+            </div>
+            ))}
         </div>
-    );
+    )
 }
 
 export default Cards;
