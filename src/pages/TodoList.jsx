f29ea53beb99f978e4def0519d2a5170b3afa1ab
BarkInDaPark <matte_skater@hotmail.com>
2025-03-26 13:36:50 +0100

    added li

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -3,7 +3,7 @@ function TodoList(){
     return (
         <ul className='homepage'>
             <li>navbar:move items to middle</li>
-            <li></li>
+            <li className='taskDone'>fix hover over for cards</li>
             <li></li>
             <li></li>
             <li></li>
