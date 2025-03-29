43d940a2293fd46e1a907796e5bf50d6979e5466
BarkInDaPark <matte_skater@hotmail.com>
2025-03-29 01:27:41 +0100

    added buttons

--- src/games/Blackjack.jsx
+++ src/games/Blackjack.jsx
@@ -3,9 +3,10 @@ import './Blackjack.css';
 function Blackjack() {
     const [startButt, setstartButt] = useState("Start!");
     const [started, setstarted] = useState(false);
+    const rules = "Rules: press hit to get aditional card, if you go above 21 you become fat and lose"
 
     const buttonPressHandle = () =>{
-        setstarted(started ? false : true);
+        setstarted((prev) => !prev);
         setstartButt(started ? "Start" : "quit");
         
         console.log(started);
@@ -14,19 +15,17 @@ function Blackjack() {
     return(
         <div className = 'back'>
             <h1>blackjack</h1>
-            <h1>Rules: press hit to get aditional card, if you go above 21 you become fat and lose</h1>
+            <h1>{started ? "" : rules}</h1>
+            <div >
             <button className = "startButton" onClick={() => (buttonPressHandle())}>{startButt}</button>
+            
+            {started ? <button className = "hitButton">Hit</button> : ""}
+            {started ? <button className = "stayButton">Stay</button> : ""}
+            
+            </div>
         </div>
     );
 
 }
 export default Blackjack;
 
-function Start(){
-    console.log("start")
-    return (
-        <div>
-            <h1>Started!</h1>
-        </div>
-    );
-}
