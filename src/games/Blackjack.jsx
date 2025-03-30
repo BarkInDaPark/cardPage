61ccf24fdcffb03c4514f1b45844c0a62779683f
BarkInDaPark <matte_skater@hotmail.com>
2025-03-30 12:37:36 +0200

    adding card value

--- src/games/Blackjack.jsx
+++ src/games/Blackjack.jsx
@@ -6,28 +6,44 @@ function Blackjack() {
     const [started, setstarted] = useState(false);
     const [playerScore, setplayerScore] = useState(0);
     const [dealerScore, setdealerScore] = useState(0);
+    const [playerCards, setPlayerCards] = useState(0);
     const rules = "Rules: press hit to get aditional card, if you go above 21 you become fat and lose"
 
-    const buttonPressHandle = () =>{
+    const handleCardValue = (card) => {
+        setplayerScore((prevScore) => prevScore + card.number);
+    }
+
+    const buttonStartQuitPressed = () =>{
+        setPlayerCards(1);
         setstarted((prev) => !prev);
         setstartButt(started ? "Start" : "quit");
         
         
         console.log(started);
     };
+    const buttonHitPressed = () =>{
+        setPlayerCards(prevplayerCards => playerCards + 1);
+    };
+    const buttonStayPressed = () =>{
+        
+    };
 
     return(
         <div className = 'back'>
             <h1>blackjack</h1>
+            <h1>Score: {playerScore}</h1>
             <h1>{started ? "" : rules}</h1>
+            {started ? 
             <div className='player-container'>
-                <h1 className='player'><Deck /></h1>
-                <h1 className='player'><Deck /></h1>
-                <h1 className='player'><Deck /></h1>
+                <h1 className='player'>{playerCards >= 1 ? <Deck onCardDraw={handleCardValue}/> : ""}</h1>
+                <h1 className='player'>{playerCards >= 2 ? <Deck onCardDraw={handleCardValue}/> : ""}</h1>
+                <h1 className='player'>{playerCards >= 3 ? <Deck onCardDraw={handleCardValue}/> : ""}</h1>
             </div>
+            :
+            ""};
             <div >
-                <button className = "startButton" onClick={() => (buttonPressHandle())}>{startButt}</button>
-                {started ? <button className = "hitButton">Hit</button> : ""}
+                <button className = "startButton" onClick={() => (buttonStartQuitPressed())}>{startButt}</button>
+                {started && playerCards < 4 ? <button className = "hitButton" onClick={buttonHitPressed}>Hit</button> : ""}
                 {started ? <button className = "stayButton">Stay</button> : ""}
             <div></div>
             
