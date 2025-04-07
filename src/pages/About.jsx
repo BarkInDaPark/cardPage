39d4315181c9631891d3a2ac03b403ba023d723e
BarkInDaPark <matte_skater@hotmail.com>
2025-04-07 12:37:02 +0200

    added text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
 function About() {
     return (
         <div>
-            <h1>this is about page</h1>
+            <h1>About us:</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor feugiat lorem, sit amet rutrum massa sodales facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus eu aliquet felis. Nunc eget ornare orci. Maecenas id purus mattis, tincidunt tellus a, gravida mi. Nunc pretium imperdiet bibendum. Nullam fermentum tellus nec arcu eleifend, in fermentum metus aliquet. Sed sagittis condimentum commodo. Maecenas ornare, risus vel egestas egestas, sem massa interdum nibh, vel consectetur nisl justo id erat. Integer ut auctor dolor. Pellentesque suscipit varius ex, eu faucibus leo ultricies blandit. Mauris bibendum, urna in dignissim consequat, odio lectus ultrices ex, at placerat justo mauris non ipsum.</p>
         </div>
     )
 }
